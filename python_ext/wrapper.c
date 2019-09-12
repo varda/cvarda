5c80e59f5dc0a47c56afab26f08379a764b98353
jkvis <j.k.vis@lumc.nl>
2019-09-12 12:06:59 +0200

    Versioning

--- python_ext/wrapper.c
+++ python_ext/wrapper.c
@@ -26,20 +26,12 @@ static PyTypeObject RegionTable =
 }; // RegionTable
 
 
-static PyMethodDef methods[] =
-{
-
-    {NULL, NULL, 0, NULL}  // sentinel
-}; // methods
-
-
 static struct PyModuleDef module =
 {
     .m_base = PyModuleDef_HEAD_INIT,
     .m_name = "cvarda",
     .m_doc = "Varda2 Variant frequency database C library",
-    .m_size = -1,
-    .m_methods = methods
+    .m_size = -1
 }; // module
 
 
@@ -57,8 +49,29 @@ PyInit_cvarda(void)
         return NULL;
     } // if
 
+    int major = 0;
+    int minor = 0;
+    int patch = 0;
+    vrd_version(&major, &minor, &patch);
+
+    if (0 > PyModule_AddIntConstant(mod, "VERSION_MAJOR", major))
+    {
+        return NULL;
+    } // if
+    if (0 > PyModule_AddIntConstant(mod, "VERSION_MINOR", minor))
+    {
+        return NULL;
+    } // if
+    if (0 > PyModule_AddIntConstant(mod, "VERSION_PATCH", patch))
+    {
+        return NULL;
+    } // if
+
     Py_INCREF(&RegionTable);
-    PyModule_AddObject(mod, "RegionTable", (PyObject*) &RegionTable);
+    if (0 > PyModule_AddObject(mod, "RegionTable", (PyObject*) &RegionTable))
+    {
+        return NULL;
+    } // if
 
     return mod;
 } // PyInit_cvarda
