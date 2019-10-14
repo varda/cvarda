ae6c0501b2180d476fca5f29333a9bb1ca147ac8
jkvis <j.k.vis@lumc.nl>
2019-10-14 14:49:54 +0200

    Version 1.1.4

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, LUMC'
 author = 'LUMC'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.1'
+release = '1.1.4'
 
 
 # -- General configuration ---------------------------------------------------
