8d1207d4d5cdb3f0bdebf9da47c8fdcc722603c9
jkvis <j.k.vis@lumc.nl>
2019-10-17 14:41:58 +0200

    Revert "Revert "Setting up for deletion""
    This reverts commit 6bc2ee9b0f8aa2062f152d35c31dbae1137884d4.

--- src/main.c
+++ src/main.c
@@ -6,16 +6,7 @@
 
 #include "../include/varda.h"   // VRD_*, vrd_*
 
-
-enum
-{
-    SEQ_CAPACITY = 100000,
-    SEQ_SIZE_CAPACITY = 1000000,
-    REF_CAPACITY = 1000,
-    REF_SIZE_CAPACITY = 10000,
-    TREE_CAPACITY = 1 << 24
-}; // constants
-
+#include "../include/snv_tree.h"
 
 int
 main(int argc, char* argv[])
@@ -23,110 +14,34 @@ main(int argc, char* argv[])
     (void) argc;
     (void) argv;
 
-    vrd_Cov_Table* restrict cov = NULL;
-    vrd_SNV_Table* restrict snv = NULL;
-    vrd_MNV_Table* restrict mnv = NULL;
-    vrd_Seq_Table* restrict seq = NULL;
-    FILE* restrict stream = NULL;
-
-    cov = vrd_cov_table_init(REF_CAPACITY, REF_SIZE_CAPACITY, TREE_CAPACITY);
-    if (NULL == cov)
+    vrd_SNV_Tree* restrict tree = vrd_snv_tree_init(1000);
+    if (NULL == tree)
     {
-        (void) fprintf(stderr, "vrd_cov_table_init() failed\n");
+        (void) fprintf(stderr, "vrd_snv_tree_init() failed\n");
         goto error;
     } // if
 
-    snv = vrd_snv_table_init(REF_CAPACITY, REF_SIZE_CAPACITY, TREE_CAPACITY);
-    if (NULL == snv)
+    for (int i = 0; i < 20; ++i)
     {
-        (void) fprintf(stderr, "vrd_snv_table_init() failed\n");
-        goto error;
-    } // if
+        int position = rand() % 100;
 
-    mnv = vrd_mnv_table_init(REF_CAPACITY, REF_SIZE_CAPACITY, TREE_CAPACITY);
-    if (NULL == mnv)
-    {
-        (void) fprintf(stderr, "vrd_mnv_table_init() failed\n");
-        goto error;
-    } // if
+        (void) fprintf(stderr, "insert: %d\n", position);
 
-    seq = vrd_seq_table_init(SEQ_CAPACITY, SEQ_SIZE_CAPACITY);
-    if (NULL == seq)
-    {
-        (void) fprintf(stderr, "vrd_seq_table_init() failed\n");
-        goto error;
-    } // if
+        if (NULL == vrd_snv_tree_insert(tree, position, 0, 0, 0))
+        {
+            (void) fprintf(stderr, "vrd_snv_tree_insert() failed\n");
+            goto error;
+        } // if
+    } // for
 
-    errno = 0;
-    stream = fopen("../data/depth10.bed", "r");
-    if (NULL == stream)
-    {
-        perror("fopen()");
-        goto error;
-    } // if
+    vrd_snv_tree_remove(tree, 40);
 
-    size_t const cov_count = vrd_coverage_from_file(stream, cov, 0);
-
-    errno = 0;
-    if (0 != fclose(stream))
-    {
-        perror("fclose()");
-        goto error;
-    } // if
-
-    (void) fprintf(stderr, "Covered regions: %zu\n", cov_count);
-
-    errno = 0;
-    stream = fopen("../data/CGND-HDA-02308_single.varda.csv", "r");
-    if (NULL == stream)
-    {
-        perror("fopen()");
-        goto error;
-    } // if
-
-    size_t const var_count = vrd_variants_from_file(stream, snv, mnv, seq, 0);
-
-    errno = 0;
-    if (0 != fclose(stream))
-    {
-        perror("fclose()");
-        goto error;
-    } // if
-
-    (void) fprintf(stderr, "Variants: %zu\n", var_count);
-
-    errno = 0;
-    stream = fopen("../data/CGND-HDA-02308_single.varda.csv", "r");
-    if (NULL == stream)
-    {
-        perror("fopen()");
-        goto error;
-    } // if
-
-    size_t const ann_count = vrd_annotate_from_file(stdout, stream, cov, snv, mnv, seq, NULL);
-
-    errno = 0;
-    if (0 != fclose(stream))
-    {
-        perror("fclose()");
-        goto error;
-    } // if
-
-    (void) fprintf(stderr, "Annotated: %zu\n", ann_count);
-
-    vrd_cov_table_destroy(&cov);
-    vrd_snv_table_destroy(&snv);
-    vrd_mnv_table_destroy(&mnv);
-    vrd_seq_table_destroy(&seq);
+    vrd_snv_tree_destroy(&tree);
 
     return EXIT_SUCCESS;
 
 error:
-
-    vrd_cov_table_destroy(&cov);
-    vrd_snv_table_destroy(&snv);
-    vrd_mnv_table_destroy(&mnv);
-    vrd_seq_table_destroy(&seq);
+    vrd_snv_tree_destroy(&tree);
 
     return EXIT_FAILURE;
 } // main
