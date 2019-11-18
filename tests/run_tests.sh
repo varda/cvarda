79d2aa75fb8a98dc2b01fe3ca5df8ba534e04d82
jkvis <j.k.vis@lumc.nl>
2019-11-18 16:35:47 +0100

    Added assert in templated tables

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,7 +8,7 @@ for target in ${1}; do
     ret=${?}
     if [ ${ret} -ne 0 ]; then
         FAIL=1
-        echo -e "\e[91mFAILED\e[39m"
+        echo -e "\e[31mFAILED\e[39m"
     else
         echo -e "\e[32mPASSED\e[39m"
     fi
