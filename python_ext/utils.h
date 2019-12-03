e1a75e19c06f77031b1d4be67dfa06bdc5b68501
jkvis <j.k.vis@lumc.nl>
2019-12-03 13:45:26 +0100

    Removed enum in Python wrapper

--- python_ext/utils.h
+++ python_ext/utils.h
@@ -5,15 +5,14 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>     // PyObject
 
+#include <stddef.h>     // size_t
+
 #include "../include/avl_tree.h"    // vrd_AVL_Tree
 
 
-enum
-{
-    CFG_REF_CAPACITY = 1000,
-    CFG_SEQ_CAPACITY = 100000,
-    CFG_TREE_CAPACITY = 1 << 24
-}; // constants
+static size_t const CFG_REF_CAPACITY = 1000;
+static size_t const CFG_SEQ_CAPACITY = 100000;
+static size_t const CFG_TREE_CAPACITY = 1 << 24;
 
 
 vrd_AVL_Tree*
