7d1ec342c00bc8cc282c1a1b9427f14324cca77d
Mark Santcroos <m.a.santcroos@lumc.nl>
2019-12-10 17:13:24 +0100

    Test for identity and not stabbing.

---
+++ python_ext/tests/test_mnv.py
@@ -0,0 +1,21 @@
+import cvarda
+
+
+def test_mnv_common_end():
+    snv_table = cvarda.SNVTable()
+    mnv_table = cvarda.MNVTable()
+    seq_table = cvarda.SequenceTable()
+
+    index = seq_table.insert("")
+
+    mnv_table.insert("chr1", 1, 4, 1, 1, index, 1)
+    mnv_table.insert("chr1", 2, 4, 1, 1, index, 2)
+
+    assert mnv_table.diagnostics() == \
+        {'chr1': {'height': 2, 'entry_size': 32, 'entries': 2}}
+
+    assert mnv_table.query("chr1", 1, 4, index) == 1
+
+    assert mnv_table.query("chr1", 2, 4, index) == 1
+
+    assert mnv_table.query("chr1", 3, 4, index) == 0
